c20708cf0718ef0571aed05747064b7936e35284
liyu <smile108205@vip.qq.com>
2022-01-12 10:21:00 +0800

    SwaggerConfig代码修改...

--- src/main/java/com/xihua/config/SwaggerConfig.java
+++ src/main/java/com/xihua/config/SwaggerConfig.java
@@ -4,13 +4,19 @@ import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrap
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Swagger config
@@ -27,13 +33,21 @@ public class SwaggerConfig {
      */
     @Bean
     public Docket createRestApi(){
+
+        ParameterBuilder tokenParam = new ParameterBuilder();
+        List<Parameter> parameterList = new ArrayList<>();
+        Parameter paramBuild = tokenParam.name("Authorization").description("请求头信息").modelRef(new ModelRef("String"))
+                .parameterType("header").required(true).build();
+        parameterList.add(paramBuild);
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .pathMapping("/")
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.xihua.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(parameterList);
     }
 
     private ApiInfo apiInfo(){
